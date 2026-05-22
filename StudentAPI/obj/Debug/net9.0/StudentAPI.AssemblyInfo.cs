83876a870dc96d908ffda00f44e7b33d154682c1
mahendra00121 <mahendrakarsodpatidar@gmail.com>
2026-05-22 18:25:58 +0530

    Added latest changes

--- StudentAPI/obj/Debug/net9.0/StudentAPI.AssemblyInfo.cs
+++ StudentAPI/obj/Debug/net9.0/StudentAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c13f87dfcfb4dc8d552007390920218284298077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d8d0dc8c8cadba46e45e3f5109d7235564fe326")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
